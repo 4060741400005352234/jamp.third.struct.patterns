a74625538f90abc41ce911b5c10daebad913e005
4060741400005352234 <Slony6ko>
2015-12-21 00:53:25 +0600

    Decorator - refactor

--- src/decorator/impl/BorderDecorator.java
+++ src/decorator/impl/BorderDecorator.java
@@ -1,23 +1,18 @@
 package decorator.impl;
 
 import javax.swing.*;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 
 public class BorderDecorator extends JComponent {
 
-    private final JComponent component;
-
     public BorderDecorator(JComponent component) {
-        this.component = component;
         this.setLayout(new BorderLayout());
+        EmptyBorder ep = new EmptyBorder(4,4,4,4);
+        LineBorder lb = new LineBorder(Color.black, 2);
+        component.setBorder(new CompoundBorder(lb, ep));
         this.add(component);
-    }
-
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-        int height = this.getHeight();
-        int width = this.getWidth();
-        g.drawRect(0, 0, width - 1, height - 8);
     }
 }
